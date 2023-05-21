14cde524d46bb7160598a38d5decf8d8f5b8e55d
Francisco Meira <fmeirajunior@gmail.com>
2023-05-21 20:34:59 -0300

    Update project react

---
+++ src/pages/ReactCountriesPage.jsx
@@ -0,0 +1,11 @@
+import Header from '../components/Header';
+import Main from '../components/Main';
+
+export default function ReactCountriesPage() {
+  return (
+    <div>
+      <Header>React-Countries</Header>
+      <Main>TESTE</Main>
+    </div>
+  );
+}
